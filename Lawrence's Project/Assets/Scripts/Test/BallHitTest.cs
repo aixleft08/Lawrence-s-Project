a4542a97981862ea580672a3d9142a14def8e82f
Moddwyn <nathaliaandaya@gmail.com>
2022-12-09 18:08:39 -0800

    Added bounce

---
+++ Lawrence's Project/Assets/Scripts/Test/BallHitTest.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallHitTest : MonoBehaviour
+{
+    public Rigidbody ball;
+    public Transform target;
+    public Camera cam;
+    public float force;
+    public LayerMask layerMask;
+
+    RaycastHit hit;
+    Ray ray;
+
+    void Update()
+    {
+        ray = cam.ViewportPointToRay (new Vector3(0.5f,0.5f,0));
+        if(Physics.Raycast(ray, out hit, 100, layerMask))
+        {
+            target.position = hit.point + (hit.normal * 0.025f);
+            target.rotation = Quaternion.FromToRotation (Vector3.up, hit.normal);
+            Debug.DrawLine(cam.transform.position, hit.point, Color.red);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        ball.AddForce(-(ball.position-target.position) * force, ForceMode.Impulse);
+    }
+}
